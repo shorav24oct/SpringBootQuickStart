69c17e28a527f1311b3c8b54fe264d65cbf1b617
shoravs <sharmasaurav142@gmail.com>
2022-09-15 09:13:25 +0530

    add API's for update and delete the topics

--- Course-API/src/main/java/com/org/springboot/app/topic/controller/TopicController.java
+++ Course-API/src/main/java/com/org/springboot/app/topic/controller/TopicController.java
@@ -3,8 +3,10 @@ package com.org.springboot.app.topic.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,10 +29,20 @@ public class TopicController {
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
-	
+
 	@PostMapping("/topics")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
+	}
+
+	@PutMapping("/topics/{id}")
+	public void updateTopic(@PathVariable String id, @RequestBody Topic topic) {
+		topicService.updateTopic(id, topic);
+	}
+
+	@DeleteMapping("/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
 	}
 
 }
--- Course-API/src/main/java/com/org/springboot/app/topic/service/TopicService.java
+++ Course-API/src/main/java/com/org/springboot/app/topic/service/TopicService.java
@@ -12,7 +12,8 @@ import com.org.springboot.app.topic.Topic;
 public class TopicService {
 
 	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-			new Topic("spring", "spring framework", "spring F/W description"), new Topic("java", "J2SE", "core java"),
+			new Topic("spring", "spring framework", "spring F/W description"), 
+			new Topic("java", "J2SE", "core java"),
 			new Topic("REST", "REST full service", "by using JAX-RS implement Jersey")));
 
 	public List<Topic> getAllTopics() {
@@ -25,6 +26,19 @@ public class TopicService {
 
 	public void addTopic(Topic topic) {
 		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 
 }
